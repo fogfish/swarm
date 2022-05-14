67729522ee8377164b166224cb190adfb0f6cf0d
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2022-05-14 23:33:22 +0300

    update license

--- internal/system/channels.go
+++ internal/system/channels.go
@@ -1,3 +1,11 @@
+//
+// Copyright (C) 2021 - 2022 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the Apache License Version 2.0. See the LICENSE file for details.
+// https://github.com/fogfish/swarm
+//
+
 package system
 
 import "sync"
