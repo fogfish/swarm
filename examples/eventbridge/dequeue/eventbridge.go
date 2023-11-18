eae5c21c9ee94c9713a881854df87e3ac22665a1
fogfish <dmkolesnikov@gmail.com>
2023-11-18 13:25:59 +0200

    add eventbridge example (#41)

--- examples/eventbridge/dequeue/eventbridge.go
+++ examples/eventbridge/dequeue/eventbridge.go
@@ -9,12 +9,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log/slog"
 
 	"github.com/fogfish/swarm"
 	"github.com/fogfish/swarm/broker/eventbridge"
 	"github.com/fogfish/swarm/internal/qtest"
 	"github.com/fogfish/swarm/queue"
+	"github.com/fogfish/swarm/queue/events"
 )
 
 type User struct {
@@ -32,6 +35,11 @@ type Like struct {
 	Text string `json:"text"`
 }
 
+type EventNote swarm.Event[*Note]
+
+func (EventNote) HKT1(swarm.EventType) {}
+func (EventNote) HKT2(*Note)           {}
+
 func main() {
 	qtest.NewLogger()
 
@@ -40,6 +48,7 @@ func main() {
 	go actor[User]("user").handle(queue.Dequeue[User](q))
 	go actor[Note]("note").handle(queue.Dequeue[Note](q))
 	go actor[Like]("like").handle(queue.Dequeue[Like](q))
+	go ebus(events.Dequeue[*Note, EventNote](q))
 
 	q.Await()
 }
@@ -49,6 +58,24 @@ type actor[T any] string
 func (a actor[T]) handle(rcv <-chan *swarm.Msg[T], ack chan<- *swarm.Msg[T]) {
 	for msg := range rcv {
 		slog.Info("Event", "type", a, "msg", msg.Object)
+		ack <- msg
+	}
+}
+
+func ebus(rcv <-chan *EventNote, ack chan<- *EventNote) {
+	for msg := range rcv {
+		prefix := ""
+		switch string(msg.Type) {
+		case "note:EventCreateNote":
+			prefix = "+ |"
+		case "note:EventUpdateNote":
+			prefix = "~ |"
+		case "note:EventRemoveNote":
+			prefix = "- |"
+		}
+
+		v, _ := json.MarshalIndent(msg, prefix, " ")
+		fmt.Printf("common note > \n %s\n", v)
 		ack <- msg
 	}
 }
